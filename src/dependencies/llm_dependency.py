1b2d5295dcd96324703a10ee6624942b3a1b6cae
Mithzyl <517711766@qq.com>
2024-12-11 17:22:32 +0800

    Modified langgraph workflow for chat, deprecated function "get_llm_api" in llm_dependency.py, "create_first_chat" in llm_api.py

--- src/dependencies/llm_dependency.py
+++ src/dependencies/llm_dependency.py
@@ -1,4 +1,4 @@
-from IPython.core.hooks import deprecated
+from deprecated.sphinx import deprecated
 from fastapi import Depends
 from sqlmodel import Session
 
@@ -12,6 +12,7 @@ from services.llm_service import LlmService
 def get_llm_service(session: Session = Depends(get_session)) -> LlmService:
     return LlmService(session)
 
+@deprecated(reason="The llm api is wrapped into llm graph for llm agent workflow")
 def get_llm_api(llm_param: ChatCreateParam) -> LlmApi:
     default_model = 'qwen2:0.5b'
     if not llm_param.model:
