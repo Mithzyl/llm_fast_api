1b4c1a6ef75c65e0a69cf6a443a311babc50194a
Mithzyl <517711766@qq.com>
2024-12-22 03:12:22 +0800

    Added cunstom file loader for loading different formats of files and splitting

--- src/utils/langchain_loader.py
+++ src/utils/langchain_loader.py
@@ -1,5 +1,9 @@
+import os
+
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.document_loaders.generic import GenericLoader
+from langchain_community.document_loaders.parsers import PyPDFParser
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 
 class CustomFileLoader:
@@ -7,15 +11,27 @@ class CustomFileLoader:
     Manages different lang chain loader class
     """
     loader: GenericLoader
+    splitter: RecursiveCharacterTextSplitter
+
+    def __init__(self):
+        self.splitter = RecursiveCharacterTextSplitter(
+            chunk_size=768,
+            chunk_overlap=20,
+        )
+
 
     def load_pdf_files(self, folder_path: str):
-        pages = []
-        loader = GenericLoader.from_filesystem(folder_path, glob="**/*.pdf", show_progress=True)
-        documents = loader.load()
+        files = os.listdir(folder_path)
+        documents = []
+        for file in files[:5]:
+            if file.endswith(".pdf"):
+                print(f"loading {file}\n")
+                pages = PyPDFLoader(os.path.join(folder_path, file))
+                documents.append(pages.load_and_split(self.splitter))
 
         return documents
 
 
 if __name__ == "__main__":
     loader = CustomFileLoader()
-    loader.load_pdf_files("")
+    loader.load_pdf_files("/Users/mith/Desktop/project/llm_fast_api/src/files/paper")
